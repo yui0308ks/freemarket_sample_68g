8c17f516e91a403d4f880701975da12365564bd5
kohei-jp <kohei.mitateda@gmail.com>
2020-02-14 15:59:28 +0900

    setting cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 # config valid only for current version of Capistrano
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock '<3.12.0>'
+lock '3.12.0)'
 
 # Capistranoのログの表示に利用する
 set :application, 'freemarket_sample_68g'
@@ -16,7 +16,7 @@ set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['ssh -i freemarket_sample_68g.pem ec2-user@18.177.192.47'] 
+                  keys: ['~/.ssh/freemarket_sample_68g.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '18.177.192.47', user: 'ec2-user', roles: %w{app db web}
+server '18.177.242.24', user: 'ec2-user', roles: %w{app db web}
 
 # server-based syntax
 # ======================
