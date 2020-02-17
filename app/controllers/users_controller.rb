d4c746482b1041074dae71c0a1be6acdf8f0dcfb
Satoshi3104kou <satoshi.koumura@gmail.com>
2020-02-17 11:12:35 +0900

    マイページの実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+class UsersController < ApplicationController
+
+  def show
+    # @nickname = current_user.nickname
+
+    #あと何が必要か分からない
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "items#index"
   devise_for :users
+  resources :users, only: :show
 
 end
