df229a5488708742715d916be8362ee6b4f974ca
kohei-jp <kohei.mitateda@gmail.com>
2020-02-20 12:18:04 +0900

    TOPページ編集中

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,18 @@
 class ItemsController < ApplicationController
   def index
+    # @items = Items.all
+    @search_params = item_search_params
+    @items_search = Item.search(@search_params).includes(:prefecture)
   end
 
   def new
   end
+
+
+  private
+
+  def item_search_params
+    params.fetch(:search, {}).permit(:name)
+  end
+
 end
