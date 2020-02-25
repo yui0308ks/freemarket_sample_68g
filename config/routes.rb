ea8ea2ad5881e3ecece30d5166e77c50bf687c85
sasugadayo <sasugaff@icloud.com>
2020-02-25 18:44:43 +0900

    購入ページにて送付先等記入。

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,12 @@ Rails.application.routes.draw do
       get 'get_delivery_method'
       
     end
-    
+    resources :purchase, only: [:index] do
+      collection do
+        post 'pay', to: 'purchase#pay'
+        get 'done', to: 'purchase#done'
+      end
+    end
   end
 
   resources :signups, only: [:new, :create] do
@@ -38,18 +43,10 @@ Rails.application.routes.draw do
 
   resources :cards, only: [:new, :show] do
     collection do
-      post 'show', to: 'cards#show'
       post 'pay', to: 'cards#pay'
       post 'delete', to: 'cards#delete'
     end
   end
 
-  resources :purchase, only: [:index] do
-    collection do
-      get 'index', to: 'purchase#index'
-      post 'pay', to: 'purchase#pay'
-      get 'done', to: 'purchase#done'
-    end
-  end
 
 end
