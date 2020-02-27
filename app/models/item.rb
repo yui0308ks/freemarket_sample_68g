2a138b3a845e0fa4aa009adc3b0693be4d4ad1b1
Satoshi3104kou <satoshi.koumura@gmail.com>
2020-02-27 19:25:56 +0900

    単体テスト

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,8 @@
 class Item < ApplicationRecord
-  # validates :name, :image, :description, :category, :condition, :delivery_charge, :delivery_way, :prefecture, :delivery_day, :price, presence: true
-  # validates :name, length: { minimum: 1, maximum: 40 }
-  # validates :description, length: { minimum: 1, maximum: 1000 }
+  validates :name, :description,  :category_id, :condition, :delivery_charge_id, :prefecture_id, :delivery_day_id, :price, presence: true
+  validates :name, length: { minimum: 1, maximum: 40 }
+  validates :description, length: { minimum: 1, maximum: 1000 }
+
 
   validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   has_many :images, dependent: :destroy
