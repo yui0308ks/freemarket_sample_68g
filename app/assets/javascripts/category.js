730ac3ac6a32926821b6e091c3c4544b32a6048c
s.koumura <just.doo.it.sk@gmail.com>
2020-03-02 18:18:28 +0900

    配送方法の実装完了

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -36,7 +36,6 @@ $(function(){
   // Lカテゴリーが選択された時のアクション
   $("#parent-category").on('change', function() {
     l_cat = $(this).val()
-    console.log(l_cat)
     $("#m_category, #s_category").remove()
     
     // ajaxでリクエストを送信
@@ -45,17 +44,17 @@ $(function(){
         url: "/items/category",
         data: {l_cat: l_cat},
         dataType: 'json',
-        error : function(XMLHttpRequest, textStatus, errorThrown) {
-          console.log("ajax通信に失敗しました");
-          console.log("XMLHttpRequest : " + XMLHttpRequest.status);
-          console.log("textStatus     : " + textStatus);
-          console.log("errorThrown    : " + errorThrown.message);
-      },
-      //ajax通信成功
-      success : function(response) {
-          console.log("ajax通信に成功しました");
-          console.log(response);
-      }
+      //   error : function(XMLHttpRequest, textStatus, errorThrown) {
+      //     console.log("ajax通信に失敗しました");
+      //     console.log("XMLHttpRequest : " + XMLHttpRequest.status);
+      //     console.log("textStatus     : " + textStatus);
+      //     console.log("errorThrown    : " + errorThrown.message);
+      // },
+      // //ajax通信成功
+      // success : function(response) {
+      //     console.log("ajax通信に成功しました");
+      //     console.log(response);
+      // }
       })
       // doneメソッドでappendする
       .done(function(m_cat) {
@@ -86,9 +85,109 @@ $(function(){
       })
     })
   })
+
+  // Sカテゴリーが選択された時のアクション
+  $(document).on('change', "#s_category", function() {
+    // m_cat = $(this).val()
+    var array = ['S','M','L']
+    $("#").remove()
+
+    $.ajax({
+      type: "GET",
+      url: "/items/category",
+      data: {m_cat: m_cat},
+      dataType: 'json'
+    })
+    .done(function(s_cat) {
+      var catNum = 2
+      appendSelect(catNum)
+      s_cat.forEach(function(s_cat) {
+        appendCat(s_cat, catNum)
+      })
+    })
+  })
+
+
+
+
+
+
 })
 
 
+// $(function(){
+//   var array1 = [ '未定','らくらくメルカリ便','ゆうメール','レターパック','普通郵便（定型、定型外）',
+//   'クロネコヤマト','ゆうパック','クリックポスト','ゆうパケット' ]
+//   var array2 = ['ゆうメール','クロネコヤマト','ゆうパック']
+
+//   function appendOption(method){
+//     var html = `<option value="${method}">${method}</option>`;
+//     return html;
+//   }
+
+//   $("#delivery_charge").on('change',function(){
+//     var delivery_parentCategory = ""
+//     delivery_parentCategory = document.getElementById('delivery_charge').value;
+//     if (delivery_parentCategory  != "選択してください"){
+//       // console.log(delivery_parentCategory)
+//     $.ajax({
+//         url: 'get_delivery',
+//         type: 'GET',
+//         data: { parent_name: delivery_parentCategory },
+//         dataType: 'json' ,
+//       //   error : function(XMLHttpRequest, textStatus, errorThrown) {
+//       //     console.log("ajax通信に失敗しました");
+//       //     console.log("XMLHttpRequest : " + XMLHttpRequest.status);
+//       //     console.log("textStatus     : " + textStatus);
+//       //     console.log("errorThrown    : " + errorThrown.message);
+//       // },
+//       // //ajax通信成功
+//       // success : function(response) {
+//       //     console.log("ajax通信に成功しました");
+//       //     console.log(response);
+//       // }
+//     })
+
+//       .done(function(method) {
+        
+//         $('#delivery_method-parent').remove();
+//         var methodBoxHtml = '';
+//         var insertHTML = '';
+        
+//         if (delivery_parentCategory == "送料込み（出品者負担）"){
+//           array1.forEach(function(method){
+//           insertHTML += appendOption(method);
+       
+//           });
+//         }else (delivery_parentCategory == "着払い（購入者負担）");{
+//             array2.forEach(function(method){
+//             insertHTML += appendOption(method);
+           
+//             });
+//           }
+//         methodBoxHtml = `<div class="listing-product-delivery-method" id="delivery_method-parent"> 
+//                           <label class='listing-default__label'> 配送の方法</label> 
+//                           <span class='listing-default--require'>必須</span> 
+//                             <div class='listing-select-wrapper__box'>
+//                               <div class='listing-select-wrapper'></div>
+//                                 <select class="listing-select-wrapper__box--select" id="delivery_charge">
+//                                   <option value="---">選択してください</option>
+//                                   ${insertHTML}
+//                                 </select>
+//                                 <i class='fas fa-chevron-down listing-select-wrapper__box--arrow-down'></i>
+//                               </div>
+//                             </div>
+//                           </div>`;
+//       $('.delivery_charge').append(methodBoxHtml);
+
+//         console.log(delivery_parentCategory)
+//       })
+//     }
+//     else {$('#delivery_method-parent').remove();}
+
+//   });
+
+// });
 
 
 
