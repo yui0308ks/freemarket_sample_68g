fb2b36e9c5d34ca67fab5496a9ed6d6238c184e7
kohei-jp <kohei.mitateda@gmail.com>
2020-03-04 12:46:17 +0900

    user.rb バリ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-        #  デプロイで試験中のため8~21行目を一時的にコメントアウト
         kana = /\A([ァ-ン]|ー)+\z/
         VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{7,30}+\z/i
 
         validates :nickname,                presence: true, length: {maximum: 20}
@@ -18,9 +18,8 @@ class User < ApplicationRecord
         validates :birthday_month_id,       numericality: { greater_than: 0 }
         validates :birthday_day_id,         numericality: { greater_than: 0 }
         validates :email,                   presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+
         validates :password,                presence: true, format: { with: VALID_PASSWORD_REGEX , message: 'は英字と数字両方を含むパスワードを設定してください'}
-        
-        validates :password_confirmation,   presence: true, length: {minimum: 7, maximum: 30}
 
         
         
