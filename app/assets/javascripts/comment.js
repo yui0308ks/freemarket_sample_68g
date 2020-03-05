7b2d20b9bd58ee1a0d110daef407bc80abb2ff79
kohei-jp <kohei.mitateda@gmail.com>
2020-03-05 14:40:24 +0900

    コメント投稿機能

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,19 +1,19 @@
 $(function(){
   function buildHTML(comment){
     var html = `<li class="clearfix">
-    <div class="comment-left">
-      ${comment.user.nickname}
-    </div>
-    <div class="comment-right">
-      ${comment.text}
-    </div>
-    </li>`
+                  <div class="comment-left">
+                    ${comment.user_name}
+                  </div>
+                  <div class="comment-right">
+                    ${comment.text}
+                  </div>
+                </li>`
     return html;
   }
   $('#new_comment').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action')
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
@@ -24,7 +24,8 @@ $(function(){
     })
     .done(function(data){
       var html = buildHTML(data);
-      $('.comments').append(html);
+      console.log(data); 
+      $('.commentBox__inner').append(html);  //ここではないか
       $('.textbox').val('');
       $('.form__submit').prop('disabled', false);
     })
